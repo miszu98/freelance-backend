8923b5e4c56521d72db98f2efddc6f2a806c3ef8
Michal Malek <michal.malek98@wp.pl>
2022-02-06 23:42:23 +0100

    init

---
+++ src/test/java/pl/malek/freelancebackend/FreelanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.malek.freelancebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
