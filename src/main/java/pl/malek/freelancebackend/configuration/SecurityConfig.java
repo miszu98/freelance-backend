d40b7de1f6281be407160b599b76b653da077609
Michal Malek <michal.malek98@wp.pl>
2022-02-07 14:12:49 +0100

    added cors config

--- src/main/java/pl/malek/freelancebackend/configuration/SecurityConfig.java
+++ src/main/java/pl/malek/freelancebackend/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package pl.malek.freelancebackend.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,17 +9,36 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("front.url")
+    private String frontUrl;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests()
+        http.cors().configurationSource(corsConfigurationSource())
+                .and()
+                .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/users/").permitAll();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200/"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
--- src/main/java/pl/malek/freelancebackend/service/UserService.java
+++ src/main/java/pl/malek/freelancebackend/service/UserService.java
@@ -2,12 +2,13 @@ package pl.malek.freelancebackend.service;
 
 import org.springframework.validation.BindingResult;
 import pl.malek.freelancebackend.dto.User;
-import pl.malek.freelancebackend.entity.UserEntity;
 import pl.malek.freelancebackend.exception.UserAccountValidationException;
 import pl.malek.freelancebackend.exception.UserAlreadyExistException;
 
 public interface UserService {
 
     User register(User user, BindingResult result) throws UserAccountValidationException, UserAlreadyExistException;
+
     boolean checkIfUserExist(String email);
+
 }
